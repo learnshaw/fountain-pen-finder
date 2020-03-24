9fe2d9d1dbf7a0d14e48cccdf00cce59ff0ef34b
Lindsay Earnshaw <learnshaw@gmail.com>
2020-03-24 14:07:53 -0400

    changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
+import pen from './images/spilled-ink.jpg';
+import Subscribe from './sign-up/sign-up.js';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <p className="title">
+          Fountain Pen Finder
+      </p>
+      
+      <div className='flex'>
+      <p className="about intro">
+        Looking for a better way to keep track of your growing fountain pen collection? <br></br>Tired of losing your expensive pens? 
+        
+      </p>
+      <img src={pen} className="image"/>
+      </div>
+        
+        
+      <p className='about try'>
+        Try Fountain Pen Finder! An innovative new collection management system specifically for fountain pen collectors. 
+      </p>
+        <Subscribe/>
       </header>
     </div>
+
+  
   );
 }
 
---
+++ src/sign-up/sign-up.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import '../App.css';
+
+function SignUp() {
+
+    return(
+        <div>
+
+
+    <p className='about'>Sign Up Here for More Information</p>
+        <form action="http://www.focuspocus.io/magic/cad347fd65ed789d83db30221d33830b" method="POST">
+            <fieldset className="email-input-flex">
+                <input className ="email-input" type="email" name="email" id="email" required="true" placeholder="email address" /><input className="subscribe-button" type="submit" value="subscribe"/>
+                {/* <label for="email" className="visuallyhidden">email address</label> */}
+            </fieldset>     
+        </form>
+
+
+</div>
+        );
+}
+
+export default SignUp;
