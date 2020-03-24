cbb003dcc26bea12eff244d81a6bde8ac0d1cb22
Lindsay Earnshaw <learnshaw@gmail.com>
2020-03-24 15:06:26 -0400

    further updates

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       
       <div className='flex'>
       <p className="about intro">
-        Looking for a better way to keep track of your growing fountain pen collection? <br></br>Tired of losing your expensive pens? 
+        Looking for a better way to keep track of your growing fountain pen collection? <br></br> <br></br><br></br>Tired of losing your expensive pens? 
         
       </p>
       <img src={pen} className="image"/>
@@ -21,7 +21,7 @@ function App() {
         
         
       <p className='about try'>
-        Try Fountain Pen Finder! An innovative new collection management system specifically for fountain pen collectors. 
+        Try Fountain Pen Finder! An innovative new collection management and GPS tracking system specifically for fountain pen collectors. 
       </p>
         <Subscribe/>
       </header>
--- src/sign-up/sign-up.js
+++ src/sign-up/sign-up.js
@@ -7,10 +7,11 @@ function SignUp() {
         <div>
 
 
-    <p className='about'>Sign Up Here for More Information</p>
+    <p className='about'>Sign up below to be the first to know when the system is launched</p>
         <form action="http://www.focuspocus.io/magic/cad347fd65ed789d83db30221d33830b" method="POST">
             <fieldset className="email-input-flex">
-                <input className ="email-input" type="email" name="email" id="email" required="true" placeholder="email address" /><input className="subscribe-button" type="submit" value="subscribe"/>
+                <input className ="email-input" type="email" name="email" id="email" required="true" placeholder="email address" />
+                <input className="subscribe-button" type="submit" value="subscribe"/>
                 {/* <label for="email" className="visuallyhidden">email address</label> */}
             </fieldset>     
         </form>
