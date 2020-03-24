b498e93b8ba61d206099c185cd75aca7b3f007cf
Lindsay Earnshaw <learnshaw@gmail.com>
2020-03-24 16:18:15 -0400

    fixed typo

--- src/sign-up/sign-up.js
+++ src/sign-up/sign-up.js
@@ -5,7 +5,7 @@ function SignUp() {
 
     return(
         <div>
-            <p className='about'>Sign up below to be the first to know when the system is launched</p>
+            <p className='about'>Sign up below to be the first to know when the system is launched.</p>
                 <form action="http://www.focuspocus.io/magic/cad347fd65ed789d83db30221d33830b" method="POST">
                     <fieldset className="email-input-flex">
                         <input className ="email-input" type="email" name="email" id="email" required="true" placeholder="email address" />
